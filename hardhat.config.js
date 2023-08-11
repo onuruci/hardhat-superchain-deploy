459e3449c7fab0a1964056b0e9cd8b16aa7bbdb0
onuruci <onur.yuce@metu.edu.tr>
2023-08-11 12:40:14 +0300

    task to get private key
    gets json wallet file path. prompts user for password returns private key.  Wait for user to read it then deletes the logs including private information.

--- hardhat.config.js
+++ hardhat.config.js
@@ -51,6 +51,27 @@ task("generate-deployer", async (args, hre) => {
   });
 });
 
+task("get-private-key", "Get private key of account")
+  .addParam("account", "Path to account json file")
+  .setAction(async (args, hre) => {
+
+    console.log(chalk.bold("Get private key of account\n"));
+
+    let password = prompt("Password of account: ");
+
+    let wallet_json = fs.readFileSync( args.account);
+
+    let wallet = hre.ethers.Wallet.fromEncryptedJsonSync(wallet_json, password);
+
+    console.log(chalk.bold.bgRed(wallet.privateKey));
+
+    let pressEnter =prompt("Did you save it the private key (press Enter to continue and delete logs)    ");
+    clearLines(3);
+
+    console.log(chalk.bold.green("Task completed successfully"));
+    
+});
+
 
 task("fund-deployer", "Fund the deployer account with native tokens")
   .setAction(async (args, hre) => {
