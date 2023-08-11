760090e3f9d9f2274e6b0c5effb27099db025076
onuruci <onur.yuce@metu.edu.tr>
2023-08-11 12:00:23 +0300

    fund deployer
    In orde deployment to happen, deployer EOA need natşve tokens. This task ask for user and send tokens to deployer account on each network.

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,14 +15,17 @@ task("generate-deployer", async (args, hre) => {
   let deployer = hre.ethers.Wallet.createRandom();
 
   console.log(chalk.white.bgRed.bold(deployer.mnemonic.phrase), "\n");
-  console.log("Take this 12 word phrase in a secure place and enter a password!");
-  console.log(chalk.bold("After you enter a password phrase will be deleted !\n"));
+  console.log("Take this 12 word phrase in a secure place and enter a password!\n");
+
+  console.log("Copy the private key into hardhat.config to be used!");
+  console.log(chalk.white.bgRed.bold(deployer.privateKey))
+  console.log(chalk.bold("After you enter a password phrase will be deleted !"));
 
   let pressEnter =prompt("Did you save it the phrase (press Enter)    ");
   clearLastLine()
 
   let password = prompt("Enter a password: ");
-  clearLines(7);
+  clearLines(9);
 
   let encryptedWallet = await deployer.encrypt(password);
 
@@ -49,9 +52,7 @@ extendEnvironment((hre) => {
 });
 
 
-task("superchain-deploy", "Deploy smart contracts to superchain")
-  .addParam("path", "Path to deploy script")
-  .addParam("deployer", "Path to deployer json file")
+task("fund-deployer", "Fund the deployer account with native tokens")
   .setAction(async (args, hre) => {
 
     try {
@@ -63,24 +64,54 @@ task("superchain-deploy", "Deploy smart contracts to superchain")
       let deployer = new hre.ethers.Wallet(hre.config.superchain.deployerAccount);
       let funder = new hre.ethers.Wallet(hre.config.superchain.funderAccount);
 
-      console.log(deployer);
-
-      console.log(funder);
+      console.log(chalk.bold("Funding the deployer account\n"));
 
       for(let i=0; i < networks.length ; i++) {
 
+        console.log(chalk.bold("-->Checking for balances on network "), chalk.bold.green(networks[i]));
+
         let provider = new hre.ethers.JsonRpcProvider(hre.config.networks[networks[i]].url);
-        let deployerBalance = await provider.getBalance(deployer.address);
-        let funderBalance = await hre.ethers.formatEther(await provider.getBalance(funder.address));
+        console.log("\n")
+        console.log("Connected to proivder");
+        let deployerBalance = hre.ethers.formatEther(await provider.getBalance(deployer.address));
+        let funderBalance = hre.ethers.formatEther(await provider.getBalance(funder.address));
 
         let gas =  (await provider.getFeeData()).gasPrice;
 
-        let fundToSend = await hre.ethers.formatEther((gas * BigInt(gasAmount)).toString());
+        let fundToSend = hre.ethers.formatEther((gas * BigInt(gasAmount)).toString());
 
-        console.log(fundToSend);
+        console.log(chalk.bold("Required amount of native tokens to deploy contract:  "), chalk.bold.green(fundToSend));
+        //deployerBalance >= fundToSend
+        if(false) {
+          console.log("Balance for deployer address: ", chalk.bold(deployer.address), " ", chalk.bold.green(deployerBalance));
 
-        console.log(balance);
+          console.log(chalk.bold.green("Deployer has enough amount of tokens passing to next network\n"));
+        } else {
+          console.log("Balance for deployer address: ", chalk.bold(deployer.address), " ", chalk.bold.red(deployerBalance));
+
+          console.log("Balance for funder address:  ", chalk.bold(funder.address), " ", chalk.bold.green(funderBalance),"\n");
+
+          let ifFund = prompt("Fund the deployer address (press Y and enter to continue)  ");
+
+          if(ifFund.toLowerCase() === "y") {
+            // fund the address
+            let funderConnected = funder.connect(provider);
+
+            let tx = {
+              to: deployer.address,
+              value: hre.ethers.parseEther(fundToSend)
+            }
+
+            let res = await funderConnected.sendTransaction(tx);
+
+            console.log("Tx hash to fund deployer ", chalk.bold(res.hash), "on network  ", chalk.bold.green(networks[i]));
+
+            console.log(chalk.bold.green("Tx sent for funding the deployer passing to next network\n"));
+          }
+        }
       }
+
+      console.log(chalk.bold.green("Funding phase is completed continue with deploying"));
 
       //hre.hardhatArguments.network = "hardhat";
 
@@ -93,6 +124,12 @@ task("superchain-deploy", "Deploy smart contracts to superchain")
   }
 );
 
+task("superchain-deploy", "Deploy smart contracts to superchain")
+  .addParam("path", "Path to deploy script")
+  //.addParam("deployer", "Path to deployer json file")
+  .setAction(async (args, hre) => {
+    console.log(chalk.bold.green("Deployment phase"));
+  })
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -106,10 +143,13 @@ module.exports = {
     },
     fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/olvyiXDSdK2fqK3CgEpJg4VUvaw4nQ48"
     }
   },
   superchain: {
-    networks: ["fuji"],
+    networks: ["fuji", "goerli"],
     deployerAccount: process.env.DEPLOYER_ACCOUNT,
     funderAccount: process.env.FUNDER_ACCOUNT
   },
